512e6db0d50c5fc93b06d9d087bbf53cbb5b11ce
Rohit Pawar <rohitpawar.skills@gmail.com>
2022-10-02 21:48:20 +0530

    832. Flipping an Image

--- flippingAnImage.js
+++ flippingAnImage.js
@@ -16,16 +16,16 @@ var flipAndInvertImage = function (image) {
         }
     }
 
-    // for (let i = 0; i < image.length; i++) {
-    //     for (let j = 0; j < image[i].length; j++) {
-    //         if (image[i][j] === 0) {
-    //             image[i][j] = 1;
-    //         } else {
-    //             image[i][j] = 0;
-    //         }
-    //     }
+    for (let i = 0; i < image.length; i++) {
+        for (let j = 0; j < image[i].length; j++) {
+            if (image[i][j] === 0) {
+                image[i][j] = 1;
+            } else {
+                image[i][j] = 0;
+            }
+        }
 
-    // }
+    }
     return image;
 };
 
