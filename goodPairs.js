e687475e36c44ea1f4cd536a91d5b8c36f78176d
Rohit Pawar <rohitpawar.skills@gmail.com>
2022-10-02 21:40:39 +0530

    1512. Number of Good Pairs

---
+++ goodPairs.js
@@ -0,0 +1,19 @@
+// 1512. Number of Good Pairs
+
+console.log(`_Number of Good Pairs_`);
+
+var numIdenticalPairs = function (nums) {
+    let ans = 0;
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] === nums[j]) {
+                ans++;
+            }
+        }
+    }
+    return ans;
+};
+
+console.log(numIdenticalPairs([1, 2, 3, 1, 1, 3]));
+console.log(numIdenticalPairs([1, 1, 1, 1]));
+console.log(numIdenticalPairs([1, 2, 3]));
