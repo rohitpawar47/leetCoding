6c53fc4ae0451b8ff6acd16f545c8406058ed72d
Rohit Pawar <rohitpawar.skills@gmail.com>
2022-10-05 16:59:06 +0530

    Triplets Sum (3sum)

--- tripletsSum.js
+++ tripletsSum.js
@@ -37,3 +37,39 @@ const tripletsSum = function (nums) {
 console.log(tripletsSum([-1, 0, 1, 2, -1, 4]));
 console.log(tripletsSum([0, 0, 0]));
 console.log(tripletsSum([0, 1, 1]));
+
+/*******************************/
+
+console.log(`_3sum (window method)_`);
+
+const threeSum = function (nums) {
+    let ans = [];
+    nums.sort((a, b) => a - b);
+
+    for (let i = 0; i < nums.length - 2; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+        let s = i + 1;
+        let e = nums.length - 1;
+
+        while (s < e) {
+            if (nums[i] + nums[s] + nums[e] === 0) {
+                ans.push([nums[i], nums[s], nums[e]]);
+                while (nums[s] === nums[s + 1]) s++;
+                while (nums[e] === nums[e - 1]) e--;
+                s++;
+            }
+            else if (nums[i] + nums[s] + nums[e] > 0) {
+                e--;
+            }
+            else {
+                s++;
+            }
+        }
+    }
+    return ans;
+}
+
+console.log(threeSum([-1, 0, 1, 2, -1, 4]));
+console.log(threeSum([0, 0, 0]));
+console.log(threeSum([0, 1, 1]));
+console.log(threeSum([0, 0, 0, 0]));
